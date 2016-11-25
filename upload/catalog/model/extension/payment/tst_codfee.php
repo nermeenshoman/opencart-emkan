c1da6ff702c38dde009523670859b55159fccb6f
nermeenshoman <nermeenshoman@gmail.com>
2016-11-25 14:48:33 +0200

    xlogistics extension

--- upload/catalog/model/extension/payment/tst_codfee.php
+++ upload/catalog/model/extension/payment/tst_codfee.php
@@ -7,8 +7,8 @@ class ModelExtensionPaymentTSTCODFEE extends Model {
 
 		if ($this->config->get('tst_codfee_total') > 0 && $this->config->get('tst_codfee_total') > $total) {
 			$status = false;
-		}  elseif (isset($this->session->data['shipping_method']['code']) && $this->session->data['shipping_method']['code'] != 'tst_xlogistics.tst_xlogistics') {
-                        $status = false;  
+		}  elseif (isset($this->session->data['shipping_method']['code']) && $this->session->data['shipping_method']['code'] == 'tst_xlogistics.tst_xlogistics') {
+                        $status = true;  
 		} elseif (!$this->cart->hasShipping()) {
 			$status = false;
                 } elseif (!$this->config->get('tst_codfee_geo_zone_id')) {
